e92c366566b339e4135902071ed87dec260f336d
ugur <bdhm32@gmail.com>
2025-08-31 09:13:23 +0000

    bot

---
+++ bot/bot.js
@@ -0,0 +1,14 @@
+export class Bot {
+  constructor(bot) {
+    this.bot = bot;
+    this.games = ["arcade_hide_and_seek_party_pooper", "tnt_bowspleef"]
+  }
+  
+  start() {
+    this.randomGame()
+  }
+  
+  randomGame() {
+    
+  }
+}
